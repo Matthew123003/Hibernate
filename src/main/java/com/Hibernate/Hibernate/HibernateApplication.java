25e09717bf51e5486ce4b7694b131a37eaeab96f
matthew123003 <matthew123003@gmail.com>
2024-10-04 14:10:34 -0400

    Updated Hibernate Application

--- src/main/java/com/Hibernate/Hibernate/HibernateApplication.java
+++ src/main/java/com/Hibernate/Hibernate/HibernateApplication.java
@@ -1,13 +1,32 @@
 package com.Hibernate.Hibernate;
 
+import org.hibernate.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class HibernateApplication {
 
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(HibernateApplication.class, args);
+	// }
+
 	public static void main(String[] args) {
-		SpringApplication.run(HibernateApplication.class, args);
-	}
+        // Start a Hibernate session
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        // Create and save an entity
+        YourEntityClass entity = new YourEntityClass();
+        entity.setName("Example");
+        session.save(entity);
+
+        // Commit the transaction and close the session
+        session.getTransaction().commit();
+        session.close();
+
+        // Shut down Hibernate when done
+        HibernateUtil.shutdown();
+    }
 
 }
